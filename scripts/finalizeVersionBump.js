561a690f6d6c72ea5a75e1e754661d806419e6ed
Ivan Gabriele <ivan.gabriele@gmail.com>
2021-08-23 21:59:44 +0200

    ci(npm): automate version bumping Git operations

---
+++ scripts/finalizeVersionBump.js
@@ -0,0 +1,28 @@
+/* eslint-disable import/no-extraneous-dependencies */
+
+const shelljs = require('shelljs')
+
+const VERSION = process.env.npm_package_version
+
+function run(command) {
+  shelljs.echo(`Running: \`${command}\`…`)
+  const output = shelljs.exec(command)
+
+  if (output.code !== 0) {
+    shelljs.exit(1)
+  }
+}
+
+;(() => {
+  try {
+    run(`git checkout -B ${VERSION}`)
+    run(`git push origin HEAD --tags`)
+    run(`git checkout main`)
+    run(`git fetch`)
+    run(`git reset --hard origin/main`)
+  } catch (err) {
+    shelljs.echo(`[scripts/finalizeVersionBump.js] Error: ${err.message}`)
+
+    shelljs.exit(1)
+  }
+})()

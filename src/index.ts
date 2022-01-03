7c6756959b7d680b0cb043b5f16d331c065e648e
Ivan Gabriele <ivan.gabriele@gmail.com>
2022-01-03 15:23:51 +0100

    feat: handle non-string messages

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,70 @@
-import chalk, { Chalk } from 'chalk'
+import chalk from 'chalk'
 import childProcess from 'child_process'
 import emoji from 'node-emoji'
 import os from 'os'
 
 import * as T from './types'
 
+import type { Chalk } from 'chalk'
+
 class Bhala implements T.Bhala {
   private canEmoji: boolean
 
   constructor(public readonly EMO = emoji.emoji) {
-    this.canEmoji = !this.isCmd() && process.env.CI === undefined
+    this.canEmoji = !this.isCmd() && !this.isCi()
+  }
+
+  public debug(...messages: any[]): void {
+    const output = this.generateOutput(messages, chalk.gray, this.EMO.pushpin, 'debug -')
+
+    // eslint-disable-next-line no-console
+    console.debug(output)
+  }
+
+  public error(...messages: any[]): void {
+    const output = this.generateOutput(messages, chalk.red, this.EMO.x, 'error -')
+
+    // eslint-disable-next-line no-console
+    console.error(output)
+  }
+
+  public event(...messages: any[]): void {
+    const output = this.generateOutput(messages, chalk.magenta, this.EMO.calendar, 'event -')
+
+    // eslint-disable-next-line no-console
+    console.log(output)
+  }
+
+  public info(...messages: any[]): void {
+    const output = this.generateOutput(messages, chalk.cyan, this.EMO.information_source, 'info -')
+
+    // eslint-disable-next-line no-console
+    console.info(output)
+  }
+
+  public log(...messages: any[]): void {
+    const output = this.generateOutput(messages, chalk.white, this.EMO.memo, 'log -')
+
+    // eslint-disable-next-line no-console
+    console.log(output)
+  }
+
+  public success(...messages: any[]): void {
+    const output = this.generateOutput(messages, chalk.green, this.EMO.heavy_check_mark, 'success -')
+
+    // eslint-disable-next-line no-console
+    console.log(output)
+  }
+
+  public warn(...messages: any[]): void {
+    const output = this.generateOutput(messages, chalk.yellow, this.EMO.warning, 'warning -')
+
+    // eslint-disable-next-line no-console
+    console.warn(output)
   }
 
   private generateOutput(
-    messages: string[],
+    messages: any[],
     defaultColorizer: Chalk,
     defaultPrefixIcon: string,
     defaultPrefixText: string,
@@ -52,53 +103,8 @@ class Bhala implements T.Bhala {
     }
   }
 
-  public debug(...messages: string[]): void {
-    const output = this.generateOutput(messages, chalk.gray, this.EMO.pushpin, 'debug -')
-
-    // eslint-disable-next-line no-console
-    console.debug(output)
-  }
-
-  public error(...messages: string[]): void {
-    const output = this.generateOutput(messages, chalk.red, this.EMO.x, 'error -')
-
-    // eslint-disable-next-line no-console
-    console.error(output)
-  }
-
-  public event(...messages: string[]): void {
-    const output = this.generateOutput(messages, chalk.magenta, this.EMO.calendar, 'event -')
-
-    // eslint-disable-next-line no-console
-    console.log(output)
-  }
-
-  public info(...messages: string[]): void {
-    const output = this.generateOutput(messages, chalk.cyan, this.EMO.information_source, 'info -')
-
-    // eslint-disable-next-line no-console
-    console.info(output)
-  }
-
-  public log(...messages: string[]): void {
-    const output = this.generateOutput(messages, chalk.white, this.EMO.memo, 'log -')
-
-    // eslint-disable-next-line no-console
-    console.log(output)
-  }
-
-  public success(...messages: string[]): void {
-    const output = this.generateOutput(messages, chalk.green, this.EMO.heavy_check_mark, 'success -')
-
-    // eslint-disable-next-line no-console
-    console.log(output)
-  }
-
-  public warn(...messages: string[]): void {
-    const output = this.generateOutput(messages, chalk.yellow, this.EMO.warning, 'warning -')
-
-    // eslint-disable-next-line no-console
-    console.warn(output)
+  private isCi(): boolean {
+    return typeof process !== undefined && typeof process.env !== undefined && process.env.CI !== undefined
   }
 }
 
